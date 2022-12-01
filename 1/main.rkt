c7285bf9145d09ee6070dfba84ee3c53380bf05c
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2022-12-01 11:17:23 +0100

    Day1

---
+++ 1/main.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(define (sum-invs)
+  (for/fold ([elfs '()]
+             [inventory 0]
+             #:result (cons inventory elfs))
+            ([x (in-lines)])
+       (set! x (string->number x))
+       (match x
+        [#f (values (cons inventory elfs) 0)]
+        [_ (values elfs (+ inventory x))])))
+
+(define (part1 input)
+  (with-input-from-file input
+       (lambda () (apply max (sum-invs)))))
+
+(define (part2 input)
+  (with-input-from-file input
+    (lambda () (apply + (take (sort (sum-invs) >) 3)))))
+
+
+(printf "Part 1: ~a\n" (part1 "input"))
+(printf "Part 2: ~a\n" (part2 "input"))
