566934dc49f01e0deba336112f747264d635ef8f
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2022-12-06 23:04:00 +0100

    Day 6 done

--- 6/main.rkt
+++ 6/main.rkt
@@ -1,19 +1,35 @@
 #lang racket
 (require util)
-
-(define example1 "mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+(require threading)
 
 (define (all-different buffer)
   (for/and ([x buffer])
      (= 1 (for/sum ([y buffer])
              (if (= x y) 1 0)))))
 
-(define (find-start-of-packet buffer [processed 0])
-  (if (all-different (take buffer 4))
-      (+ processed 4)
-      (find-start-of-packet (rest buffer) (+ processed 1))))
+(define (find-marker length buffer [processed 0])
+  (if (all-different (take buffer length))
+      (+ processed length)
+      (find-marker length (rest buffer) (+ processed 1))))
+
+(define (find-start-of-packet buffer)
+  (find-marker 4 buffer))
+
+(define (find-start-of-message buffer)
+  (find-marker 14 buffer))
+
+(define (solve find-func)
+  (~> "input"
+      open-input-file
+      port->string
+      string->list
+      find-func))
             
 (define (part1)
-  (find-start-of-packet (string->list (port->string (open-input-file "input")))))
+  (solve find-start-of-packet))
+
+(define (part2)
+  (solve find-start-of-message))
 
 (printf "Part 1: ~a\n" (part1))
+(printf "Part 2: ~a\n" (part2))
