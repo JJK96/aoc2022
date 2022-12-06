bed2972cd0d70171c68b88af9a992d631adb1e6c
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2022-12-06 23:12:18 +0100

    Add scaffolding

---
+++ new_day.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+highest=$(ls | grep -E "\d+" | sort | tail -n 1)
+new_dir=$(echo "$highest+1" | bc)
+mkdir -p $new_dir
+cd $new_dir
+touch input
+touch example.input
+cat > main.rkt << EOF
+#lang racket
+(require util)
+
+(define (part2 [input "example.input"])
+  (with-input-from-file input
+     (lambda ()
+        "TODO")))
+
+(define (part1 [input "example.input"])
+  (with-input-from-file input
+     (lambda ()
+        "TODO")))
+
+(printf "Part 1: ~a\n" (part1 "input"))
+(printf "Part 2: ~a\n" (part2 "input"))
+EOF
